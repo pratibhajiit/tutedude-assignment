3aea31fe79bea1e0cf9d04b305754a49ec7dab46
pratibhajiit <pratibhajiit@gmail.com>
2025-09-23 12:30:00 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,16 @@
+a= input("enter the number1 :")
+
+b= input("enter the number2 :")
+
+s =int(a)+ int(b)
+su =int(a)- int(b)
+mu =int(a)* int(b)
+di =int(a)/ int(b)
+
+print("Addition",s )
+print("Subtraction",su )
+print("Multiplication",mu)
+print("Division",di)
+
+
+
