36ffba06906c0d56f9edbb3c40e0548e689c9b57
pratibhajiit <pratibhajiit@gmail.com>
2025-09-23 12:50:04 +0530

    Update assignment 2.py

--- assignment 2.py
+++ assignment 2.py
@@ -1,3 +1,4 @@
+#task1
 a= input("enter the number1 :")
 
 b= input("enter the number2 :")
@@ -12,5 +13,12 @@ print("Subtraction",su )
 print("Multiplication",mu)
 print("Division",di)
 
+#task2
+
+x= input("Enter your first name: ")
+y= input("Enter your last name:")
+z  = x+ " "+y
+
+print("hello, " + z +" welcome to python program")
 
 
