dad4355a127252fd03d504d05e653a8d0677087a
pratibhajiit <pratibhajiit@gmail.com>
2025-09-25 14:07:45 +0530

    Add files via upload

---
+++ module 4assign partb.py
@@ -0,0 +1,10 @@
+a= int(input("ESnter the number:"))
+import math
+print("square root:",math.sqrt(a))
+math.log(a)
+print("Logarithmic: ",math.log(a))
+
+math.sin(a)
+print("sine: ",math.sin(a))
+
+
