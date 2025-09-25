cf60a86aad5e68f7cb12c8fea10f2c0d441b7781
pratibhajiit <pratibhajiit@gmail.com>
2025-09-25 14:02:32 +0530

    Update assignment 5.py

--- assignment 5.py
+++ assignment 5.py
@@ -1,3 +1,5 @@
+###part 1
+
 dict={'ram':25,'mohan':78,'Alice':85}
 
 
@@ -13,3 +15,11 @@ elif a=="Alice":
 
 else:
     print(dict.get('moh',"student not found"))
+
+##part 2
+l= [1,2,3,4,5,6,7,8,9,10]
+print("Original list:",l)
+del l[5:10]
+print("Extracted first five elementss: ",l)
+l.reverse()
+print("Reversed extracted element: ",l)
