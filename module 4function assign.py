ff50e4799536b73e858d489598a1d8b66807fcaf
pratibhajiit <pratibhajiit@gmail.com>
2025-09-25 14:09:37 +0530

    Update module 4function assign.py

--- module 4function assign.py
+++ module 4function assign.py
@@ -1,5 +1,5 @@
 
-
+##task1
 
 def factorial(n):
     if n<2:
@@ -9,6 +9,22 @@ def factorial(n):
 user_input=int(input("enter your number"))
 result=factorial(user_input)
 print("the factorial of" ,user_input,"is" ,result)
+
+
+
+##task 2
+
+
+a= int(input("ESnter the number:"))
+import math
+print("square root:",math.sqrt(a))
+math.log(a)
+print("Logarithmic: ",math.log(a))
+
+math.sin(a)
+print("sine: ",math.sin(a))
+
+
 
 
 
