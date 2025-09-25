683f0a378bc22c49f888df2e8bf030bd9a898780
pratibhajiit <pratibhajiit@gmail.com>
2025-09-25 13:55:09 +0530

    Update assignment file.py

--- assignment file.py
+++ assignment file.py
@@ -3,5 +3,29 @@ read_file=f1.read()
 print(read_file)
 f1.close()
 
+### task 2
+
+user_data= input("enter text to write to the file: ")
+file1= open('out.txt',"w")
+write_file= file1.write(user_data)
+
+print("Data successfully written to output.txt")
+file1.close()
+
+user_data2=input("enter additional text to append:")
+file1= open('out.txt',"a")
+
+file1.write("\n")
+write_file1 = file1.write(user_data2)
+print(write_file1)
+
+print("data successfully appended")
 
 
+file1.close()
+
+file1=open('out.txt','r')
+read_file1=file1.read()
+print("final content of output.txt is :")
+print(read_file1)
+file1.close()
