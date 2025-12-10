d159f27ee49a26eb5532cfe75fb49c5bf2595163
pratibhajiit <pratibhajiit@gmail.com>
2025-12-10 13:46:18 +0530

    Add files via upload

---
+++ assign 2 loop.py
@@ -0,0 +1,24 @@
+
+#  task 1
+
+
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+
+    print(number, "is an Even number")
+else:
+    print(number, "is an Odd number")
+
+
+
+
+#task 2
+print("Task-2")
+
+total=0
+for number in range(1, 51):
+
+    total += number
+
+print("The sum of number 1 to 50 is", total )
